d820512dcaedb46408937dc6fb159c4ccf1760bf
Daniel <dmartingar@uoc.edu>
2014-02-28 20:46:47 +0100

    Create premiosNobelInsertarCategoria.sql
    Insertar las categorias de Premios Nobel

---
+++ premiosNobelInsertarCategoria.sql
@@ -0,0 +1,7 @@
+Insert into categoria(cat_Id,descripcion) values ('F','Física');
+
+Insert into categoria(cat_Id,descripcion) values ('Q','Química');
+Insert into categoria(cat_Id,descripcion) values ('M','Físiología o Medicina');
+Insert into categoria(cat_Id,descripcion) values ('L','Literatura');
+Insert into categoria(cat_Id,descripcion) values ('E','Economía');
+Insert into categoria(cat_Id,descripcion) values ('P','Paz');
