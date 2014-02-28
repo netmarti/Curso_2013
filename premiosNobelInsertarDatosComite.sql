e913a3c9e192566151d7bbea0bf411f8a0dd2e43
Daniel <dmartingar@uoc.edu>
2014-02-28 20:59:27 +0100

    Create premiosNobelInsertarDatosComite.sql
    Insertar Datos Comité

---
+++ premiosNobelInsertarDatosComite.sql
@@ -0,0 +1,4 @@
+Insert into comite (cat_id, anyo,jurado_Id,cargo) values ('P','2007','1',NULL);
+Insert into comite (cat_id, anyo,jurado_Id,cargo) values ('P','2007','2','Presidente');
+Insert into comite (cat_id, anyo,jurado_Id,cargo) values ('M','2001','3','secretario');
+Insert into comite (cat_id, anyo,jurado_Id,cargo) values ('Q','2007','4','Presidente');
