34b78a0094fdaac6ee6d7aaccf5eb68c9dffef52
Daniel <dmartingar@uoc.edu>
2014-02-28 21:12:01 +0100

    Create premiosNobelDatosJurado.sql
    Insertar los datos del Jurado

---
+++ premiosNobelDatosJurado.sql
@@ -0,0 +1,4 @@
+Insert into jurado (jurado_Id,nombre) values ('1','Berge Ragnar Furre');
+Insert into jurado (jurado_Id,nombre) values ('2','Ole Danbotl Mjos');
+Insert into jurado (jurado_Id,nombre) values ('3','Hans Jornvall');
+Insert into jurado (jurado_Id,nombre) values ('4','Gunnar von Heijne');
