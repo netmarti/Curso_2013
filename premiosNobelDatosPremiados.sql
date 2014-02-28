467494cd47547534f3ab9a0aed77f9f581f7da30
Daniel <dmartingar@uoc.edu>
2014-02-28 21:14:59 +0100

    Create premiosNobelDatosPremiados.sql
    Inserta los datos de los premiados

---
+++ premiosNobelDatosPremiados.sql
@@ -0,0 +1,7 @@
+Insert into premiado (cat_Id,anyo,cod) values ('P','2007','4');
+Insert into premiado (cat_Id,anyo,cod) values ('L','2007','1');
+Insert into premiado (cat_Id,anyo,cod) values ('F','1921','2');
+Insert into premiado (cat_Id,anyo,cod) values ('Q','1911','5');
+Insert into premiado (cat_Id,anyo,cod) values ('F','1903','5');
+Insert into premiado (cat_Id,anyo,cod) values ('F','1903','6');
+Insert into premiado (cat_Id,anyo,cod) values ('F','1903','7');
