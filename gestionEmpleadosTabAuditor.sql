ff31ae32af1912d917a3043e3b9c098b6db8450a
Daniel <dmartingar@uoc.edu>
2014-02-28 21:50:31 +0100

    Create gestionEmpleadosTabAuditor.sql
    Creamos la tabla auditoria sobre la que vamos a realizar ejercicios de consulta sobre la BBDD Gestión de empleados.

---
+++ gestionEmpleadosTabAuditor.sql
@@ -0,0 +1,5 @@
+CREATE TABLE auditoria_empleados(num_emp INT PRIMARY KEY,nombreEmp VARCHAR(50),
+puesto VARCHAR(50),jefe VARCHAR(50),fecha_contratacion DATE,salario FLOAT,
+comision FLOAT,departamento INT,nnum_emp INT,nnombreEmp VARCHAR(50),
+npuesto VARCHAR(50),njefe VARCHAR(50),nfecha_contratacion DATE,nsalario FLOAT,
+ncomision FLOAT,ndepartamento INT,usuario VARCHAR(50),fechaActualizacion DATE);
