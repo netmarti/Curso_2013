d62ac73bd2cd10ca316d43c85ab2a1431e620c1e
Daniel <dmartingar@uoc.edu>
2014-02-28 21:13:01 +0100

    Create premiosNobelDatosNominados.sql
    Insertar datos de los nominados a los "premios Nobel"

---
+++ premiosNobelDatosNominados.sql
@@ -0,0 +1,7 @@
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('1','Doris Lessing','UK','1919');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('2','Albert Einstin','GER','1879');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('3','Roger B.Myerson','USA','1951');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('4','Albert A. Gore','USA','1948');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('5','Marie Curie','FR','1867');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('6','Pierre Curie','FR','1859');
+insert into nominado (cod,nombre,cod_pais,anyo_nac) values ('7','Antonie H. Becquerel','FR','1852');
