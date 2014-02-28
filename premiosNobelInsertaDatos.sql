44e09d5386288869f6667e73c81e11b2979f9264
Daniel <dmartingar@uoc.edu>
2014-02-28 21:07:07 +0100

    Create premiosNobelInsertaDatos.sql
    Inserta datos de los premios, edición, datos y contribución.

---
+++ premiosNobelInsertaDatos.sql
@@ -0,0 +1,9 @@
+
+Insert into edición (cat_Id,anyo,contribución) values ('L','2007','Experiencia Femenina');
+
+Insert into edición (cat_Id,anyo,contribución) values ('P','2007','Cambio Climático');
+Insert into edición (cat_Id,anyo,contribución) values ('F','1921','Efecto fotoeléctrico');
+Insert into edición (cat_Id,anyo,contribución) values ('Q','1911','Descubrimiento Polonio y radio');
+Insert into edición (cat_Id,anyo,contribución) values ('F','1903','Fenómenos de radiación');
+Insert into edición (cat_Id,anyo,contribución) values ('M','2001','Medicina alternativa');
+Insert into edición (cat_Id,anyo,contribución) values ('Q','2007','Radiación electrónica');
