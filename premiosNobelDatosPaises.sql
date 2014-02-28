572a36275c4d8356242ef554c2ad094cda8adfa5
Daniel <dmartingar@uoc.edu>
2014-02-28 21:14:06 +0100

    Create premiosNobelDatosPaises.sql
    Inserta los datos de los países participantes en los "premios Nobel"

---
+++ premiosNobelDatosPaises.sql
@@ -0,0 +1,5 @@
+insert into pais (cod_pais,nombre,num_premios) values ('USA','Estados Unidos','1');
+insert into pais (cod_pais,nombre,num_premios) values ('UK','Reino Unido','1');
+insert into pais (cod_pais,nombre,num_premios) values ('GER','Alemania','1');
+insert into pais (cod_pais,nombre,num_premios) values ('FR','Francia','4');
+insert into pais (cod_pais,nombre,num_premios) values ('ES','España','0');
